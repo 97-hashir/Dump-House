7e13472749ba9bbd10cd319407ed17d5470386d4
97-hashir <66321789+97-hashir@users.noreply.github.com>
2020-07-20 23:01:52 +0500

    Add files via upload

--- DumpHouse AdminPanel/src/app/user/user.component.ts
+++ DumpHouse AdminPanel/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../shared/customer.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  public chartData: Object[];
+  donors: number;
+  recipients: number;
+
+
+  constructor(public customerService: CustomerService) {
+    this.donors = 0;
+    this.recipients = 0;
+  }
+  public primaryXAxis: Object;
+
+  
+  userArray = [];
 
   ngOnInit() {
+
+
+    this.customerService.getDonations().subscribe(
+
+      list => {
+        this.userArray = list.map(item => {
+          return {
+            $key: item.key,
+            ...item.payload.val()
+
+          };
+
+        });
+        this.generateData()
+      }
+    );
+    this.primaryXAxis = { valueType: 'Category' };
+
   }
 
+  generateData() {
+
+    this.userArray.forEach((element) => {
+      if (element[0] == "r")
+        this.recipients++;
+      else
+        this.donors++;
+    });
+    this.chartData = [
+      { Users: "Recipients", history: this.recipients }, 
+      { Users: "Donors", history: this.donors }
+    ]
+    ;
+  }
 }
